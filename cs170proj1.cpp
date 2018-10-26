23aa3d4ccd9366d7277ebdc64df2097c0b4ea973
Ryan Yuzuki <ryuzu001@ucr.edu>
2018-10-26 15:55:50 -0700

    probably working, at least uniform probaly maybe

--- cs170proj1.cpp
+++ cs170proj1.cpp
@@ -9,16 +9,68 @@ struct problem{
     int algorithm;
 };  // problem will hold puzzle vector and algorithm choice
 
-bool checkedIfSolved(problem p){
-    if(p.puzzle.at(0) == 1)
-    if(p.puzzle.at(1) == 2)
-    if(p.puzzle.at(2) == 3)
-    if(p.puzzle.at(3) == 4)
-    if(p.puzzle.at(4) == 5)
-    if(p.puzzle.at(5) == 6)
-    if(p.puzzle.at(6) == 7)
-    if(p.puzzle.at(7) == 8)
-    if(p.puzzle.at(8) == 9)
+struct node{
+  vector<int> state;  // vector of ints to represent state
+  int cost;   // cost to get to state
+};
+
+node findCheapest(vector<node> nodes){
+  node cheapest;
+  cheapest.cost = 1000000000;
+  for(int i = 0; i < nodes.size(); i++){
+    if(cheapest.cost > nodes.at(i).cost){
+      cheapest = nodes.at(i);
+    }
+  }
+  return cheapest;
+} // find the cheapest given a vector of nodes
+
+void removeNode(node n, vector<node> & nodes, vector<node> & visitedNodes){
+  bool m = false;
+  for(int i = 0; i < nodes.size(); i++){
+    if(nodes.at(i).state.at(0) == n.state.at(0))
+    if(nodes.at(i).state.at(1) == n.state.at(1))
+    if(nodes.at(i).state.at(2) == n.state.at(2))
+    if(nodes.at(i).state.at(3) == n.state.at(3))
+    if(nodes.at(i).state.at(4) == n.state.at(4))
+    if(nodes.at(i).state.at(5) == n.state.at(5))
+    if(nodes.at(i).state.at(6) == n.state.at(6))
+    if(nodes.at(i).state.at(7) == n.state.at(7))
+    if(nodes.at(i).state.at(8) == n.state.at(8)){
+      visitedNodes.push_back(nodes.at(i));
+      nodes.erase(nodes.begin() + i);
+      m = true;
+    }
+  }
+  if(!m){
+    cout << "node not found\n";
+  }
+}
+
+bool isGoalState(vector<int> state){
+  if(state.at(0) == 1)
+  if(state.at(1) == 2)
+  if(state.at(2) == 3)
+  if(state.at(3) == 4)
+  if(state.at(4) == 5)
+  if(state.at(5) == 6)
+  if(state.at(6) == 7)
+  if(state.at(7) == 8)
+  if(state.at(8) == 0)
+  return true;
+  return false;
+}   // check if goal state
+
+bool checkedIfSolved(node p){
+    if(p.state.at(0) == 1)
+    if(p.state.at(1) == 2)
+    if(p.state.at(2) == 3)
+    if(p.state.at(3) == 4)
+    if(p.state.at(4) == 5)
+    if(p.state.at(5) == 6)
+    if(p.state.at(6) == 7)
+    if(p.state.at(7) == 8)
+    if(p.state.at(8) == 0)
     return true;
     return false;
 }   // simple check if solved function
@@ -27,6 +79,13 @@ void displayProblem(problem p){
     cout << p.puzzle[0] << " " << p.puzzle[1] << " " << p.puzzle[2] << "\n";
     cout << p.puzzle[3] << " " << p.puzzle[4] << " " << p.puzzle[5] << "\n";
     cout << p.puzzle[6] << " " << p.puzzle[7] << " " << p.puzzle[8] << "\n";
+}
+
+void printNode(node n){
+  cout << n.state.at(0) << " " << n.state.at(1) << " " << n.state.at(2) << "\n";
+  cout << n.state.at(3) << " " << n.state.at(4) << " " << n.state.at(5) << "\n";
+  cout << n.state.at(6) << " " << n.state.at(7) << " " << n.state.at(8) << "\n";
+  cout << "cost: " << n.cost << "\n";
 }
 
 problem assignDefaultPuzzle(){
@@ -202,7 +261,7 @@ problem getAlgorithm(problem p){
     cout << "3 A* with the Manhattan distance heuristic\n";
     int a;
     cin >> a;
-    if(a == 1){    
+    if(a == 1){
         cout << "\nWe will use the following puzzle:\n";
         displayProblem(p);
         cout << "using Uniform Cost Search.\n";
@@ -225,14 +284,197 @@ problem getAlgorithm(problem p){
     return p;
 }
 
-void uniformCostSearch(vector<int> puzzle){
-    cout << "uniform cost";
+bool isSameNode(node a, node b){
+  if(a.state.at(0) == b.state.at(0))
+  if(a.state.at(1) == b.state.at(1))
+  if(a.state.at(2) == b.state.at(2))
+  if(a.state.at(3) == b.state.at(3))
+  if(a.state.at(4) == b.state.at(4))
+  if(a.state.at(5) == b.state.at(5))
+  if(a.state.at(6) == b.state.at(6))
+  if(a.state.at(7) == b.state.at(7))
+  if(a.state.at(8) == b.state.at(8))
+  return true;
+  return false;
 }
 
-void misplacedTileHeuristic(vector<int> puzzle){
+bool isonList(node n, vector<node> v){
+  for(int i = 0; i < v.size(); i++){
+    if(isSameNode(n, v.at(i)))
+      return true;
+  }
+  return false;
+}
+
+node swap1(node n){
+  int t = n.state.at(0);
+  n.state.at(0) = n.state.at(1);
+  n.state.at(1) = t;
+  return n;
+}
+node swap2(node n){
+  int t = n.state.at(1);
+  n.state.at(1) = n.state.at(2);
+  n.state.at(2) = t;
+  return n;
+}
+node swap3(node n){
+  int t = n.state.at(0);
+  n.state.at(0) = n.state.at(3);
+  n.state.at(3) = t;
+  return n;
+}
+node swap4(node n){
+  int t = n.state.at(1);
+  n.state.at(1) = n.state.at(4);
+  n.state.at(4) = t;
+  return n;
+}
+node swap5(node n){
+  int t = n.state.at(2);
+  n.state.at(2) = n.state.at(5);
+  n.state.at(5) = t;
+  return n;
+}
+node swap6(node n){
+  int t = n.state.at(3);
+  n.state.at(3) = n.state.at(4);
+  n.state.at(4) = t;
+  return n;
+}
+node swap7(node n){
+  int t = n.state.at(4);
+  n.state.at(4) = n.state.at(5);
+  n.state.at(5) = t;
+  return n;
+}
+node swap8(node n){
+  int t = n.state.at(3);
+  n.state.at(3) = n.state.at(6);
+  n.state.at(6) = t;
+  return n;
+}
+node swap9(node n){
+  int t = n.state.at(4);
+  n.state.at(4) = n.state.at(7);
+  n.state.at(7) = t;
+  return n;
+}
+node swap10(node n){
+  int t = n.state.at(5);
+  n.state.at(5) = n.state.at(8);
+  n.state.at(8) = t;
+  return n;
+}
+node swap11(node n){
+  int t = n.state.at(6);
+  n.state.at(6) = n.state.at(7);
+  n.state.at(7) = t;
+  return n;
+}
+node swap12(node n){
+  int t = n.state.at(7);
+  n.state.at(7) = n.state.at(8);
+  n.state.at(8) = t;
+  return n;
+}
+
+vector<node> uniformExpand(node n, vector<node> nodes, vector<node> visited){
+  n.cost++;
+  node temp = swap1(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+  temp = swap2(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+  temp = swap3(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+  temp = swap4(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+  temp = swap5(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+  temp = swap6(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+  temp = swap7(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+  temp = swap8(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+  temp = swap9(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+  temp = swap10(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+  temp = swap11(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+  temp = swap12(n);
+  if(!isonList(temp, visited)){ // generated node is not visited yet
+    nodes.push_back(temp);
+  }
+
+
+  return nodes;
+}
+
+node uniformCostSearch(vector<int> puzzle){
+    node fail;
+    fail.cost = 0;
+    fail.state.push_back(0);
+    fail.state.push_back(0);
+    fail.state.push_back(0);
+    fail.state.push_back(0);
+    fail.state.push_back(0);
+    fail.state.push_back(0);
+    fail.state.push_back(0);
+    fail.state.push_back(0);
+    fail.state.push_back(0);
+    // driver for uniform cost search
+    node initial;
+    initial.state = puzzle;
+    initial.cost = 0;
+
+    vector<node> nodes;
+    vector<node> visited;
+    nodes.push_back(initial);
+    node currNode;
+
+    while(1){
+        if(nodes.size() == 0){
+          cout << "failure\n";
+          return fail;
+        }
+        currNode = findCheapest(nodes);
+        removeNode(currNode, nodes, visited);
+        if(checkedIfSolved(currNode)){
+          cout << "solved\n";
+          return currNode;
+        }
+        nodes = uniformExpand(currNode, nodes, visited);
+    }
+}
+
+node misplacedTileHeuristic(vector<int> puzzle){
     cout << "misplaced tile";
 }
-void manhattanDistanceHeuristic(vector<int> puzzle){
+node manhattanDistanceHeuristic(vector<int> puzzle){
     cout << "manhattan distance";
 }
 
@@ -240,19 +482,75 @@ int main(){
     problem p;
     p = getInput();
     p = getAlgorithm(p);
+    node n;
     if(p.algorithm == 1){
-        uniformCostSearch(p.puzzle);
+        n = uniformCostSearch(p.puzzle);
     }
     else if(p.algorithm == 2){
-        misplacedTileHeuristic(p.puzzle);
+        n = misplacedTileHeuristic(p.puzzle);
     }
-    else 
-        manhattanDistanceHeuristic(p.puzzle);
+    else
+        n = manhattanDistanceHeuristic(p.puzzle);
+
+    printNode(n);
     return 0;
+
+    // vector<node> n;
+    // node a;
+    // a.state.push_back(0);
+    // a.state.push_back(1);
+    // a.state.push_back(2);
+    // a.state.push_back(3);
+    // a.state.push_back(4);
+    // a.state.push_back(5);
+    // a.state.push_back(6);
+    // a.state.push_back(7);
+    // a.state.push_back(8);
+    //
+    // node b;
+    // b.state.push_back(8);
+    // b.state.push_back(7);
+    // b.state.push_back(6);
+    // b.state.push_back(5);
+    // b.state.push_back(4);
+    // b.state.push_back(3);
+    // b.state.push_back(2);
+    // b.state.push_back(1);
+    // b.state.push_back(0);
+    //
+    // node c;
+    // c.state.push_back(2);
+    // c.state.push_back(4);
+    // c.state.push_back(6);
+    // c.state.push_back(8);
+    // c.state.push_back(0);
+    // c.state.push_back(3);
+    // c.state.push_back(1);
+    // c.state.push_back(5);
+    // c.state.push_back(7);
+    // n.push_back(a);
+    // n.push_back(b);
+    // n.push_back(c);
+    // cout << "size of n: " << n.size() << endl;
+    // for(int i = 0; i < n.size(); i++){
+    //   for(int j = 0; j < n.at(i).state.size(); j++){
+    //     cout << n.at(i).state.at(j) << " ";
+    //   }
+    //   cout << endl;
+    // }
+    // cout << "remove node c\n";
+    // removeNode(c, n);
+    // cout << "size of n: " << n.size() << endl;
+    //
+    // for(int i = 0; i < n.size(); i++){
+    //   for(int j = 0; j < n.at(i).state.size(); j++){
+    //     cout << n.at(i).state.at(j) << " ";
+    //   }
+    //   cout << endl;
+    // }
 }
 
 
 //     cout << "1 Uniform Cost Search\n";
 //     cout << "2 A* with the Misplaced Tile heuristic\n";
 //     cout << "3 A* with the Manhattan distance heuristic\n";
-    
